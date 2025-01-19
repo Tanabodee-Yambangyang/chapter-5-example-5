0cc515d31e6ef83407d1be730037208181ab8fc8
Tanabodee Yambangyang <92779764+Tanabodee-Yambangyang@users.noreply.github.com>
2025-01-19 22:52:35 +0700

    Add files via upload

---
+++ example-5/video-streaming/src/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const fs = require("fs");
+const amqp = require('amqplib');
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
+
+const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT;
+
+//
+// Application entry point.
+//
+async function main() {
+	
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
+    
+    console.log("Connected to RabbitMQ.");
+
+    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+
+	await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
+
+    //
+    // Broadcasts the "viewed" message to other microservices.
+    //
+	function broadcastViewedMessage(messageChannel, videoPath, id) {
+	    console.log(`Publishing message on "viewed" exchange.`);
+	        
+	    const msg = { videoPath: videoPath, videoId: id };
+	    const jsonMsg = JSON.stringify(msg);
+	    messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publishes message to the "viewed" exchange.
+	}
+
+    const app = express();
+
+    app.get("/video", async (req, res) => { // Route for streaming video.
+        const id = req.query.id;        
+        let videoPath;
+        switch (id) {
+            case "1":
+                videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+                break;
+            case "2":
+                videoPath = "./videos/SampleVideo2.mp4";
+                break;
+            default:
+                return res.status(400).send("Invalid video ID.");
+        }    
+        const stats = await fs.promises.stat(videoPath);
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+    
+        fs.createReadStream(videoPath).pipe(res);
+
+        broadcastViewedMessage(messageChannel, videoPath, id); // Sends the "viewed" message to indicate this video has been watched.
+    });
+
+    app.listen(PORT, () => {
+        console.log("Microservice online.");
+    });
+}
+
+main()
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
